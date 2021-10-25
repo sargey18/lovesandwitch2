81fef43a16f6ba76a4386d2ca2200f02075196ec
Ryan Sargeson <ryanteachviet@gmail.com>
2021-10-25 21:09:35 +0000

    collect sales data for last 5 markets in lists of colums

--- run.py
+++ run.py
@@ -82,8 +82,19 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
-
-
+def get_last_5_entries_sales():
+    """
+    Collects collums of data from sales worksheets, collecting 
+    the last 5 entries for each sandwitch and returns the data 
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
 
 
 def main():
