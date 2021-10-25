2d5e68529a958963371964616750359edaea3d8b
Ryan Sargeson <ryanteachviet@gmail.com>
2021-10-25 12:00:55 +0000

    added the calculated surplus data to the surplus worksheet

--- run.py
+++ run.py
@@ -58,9 +58,20 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
+    
+    
+
+def update_surplus_worksheet(data):
+    """
+    update surplus worksheet add new row with the list data provided 
+    """
+    print("updating surplus worksheet\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully.\n")
 
 
-def calculate_surpluse_data(sales_row):
+def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type
     
@@ -68,7 +79,7 @@ def calculate_surpluse_data(sales_row):
     -Positive surplis indicates waste
     - Negative surplus indicates extra made when stock was sold out 
     """
-    print("calculating surpluse data...\n")
+    print("calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
 
@@ -89,8 +100,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    new_surplus_data = calculate_surpluse_data(sales_data)
-    print(new_surplus_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
     
     
 print("welcome to love sandwiches data automation")
